da037168d06fabef179cc4080168ffbde7c408bc
koyaaoyama <aoyama.ko0920@gmail.com>
2024-08-07 22:07:38 +0900

    Implement server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Go")
+	fmt.Println("Starting server on :8080")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Println("aaaaaaaaaaaaaaaaa")
+		fmt.Fprintf(w, "Hello, World!aaaaaaaaaa")
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
